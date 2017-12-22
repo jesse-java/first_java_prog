32373a55f323dfd7b54739cced06acf4f4efe2ba
Jesse Naldo <naldojesse@jnaldoweb.com>
2017-12-22 17:33:26 -0500

    initial commit

---
+++ FirstJavaProgram.java
@@ -0,0 +1,7 @@
+public class FirstJavaProgram {
+    public static void main(String[] args) {
+        System.out.println("Jesse Naldo");
+        System.out.println(24);
+        System.out.println("Seattle");
+    }
+}
