2b01f65201dba5a6a67b9011007aae7512c89ad0
Jesse Naldo <naldojesse@jnaldoweb.com>
2018-01-17 16:43:31 -0500

    removed unncessary code

--- ExceptionEx.java
+++ ExceptionEx.java
@@ -4,15 +4,6 @@ import java.util.ArrayList;
 class ExceptionEx {
 
 
-    // public static <E> void GenericCastType(E ) {
-    //     // for (E element: inputArray) {
-    //         // System.out.printf("%s ", element );
-    //     // }
-    //     System.out.println(E);
-    // }
-
-
-
     public static void main(String[] args) {
        ArrayList<Object> myList = new ArrayList<Object>();
        myList.add("13");
@@ -40,16 +31,15 @@ class ExceptionEx {
         myList2.add(48);
         myList2.add("Goodbye World");
 
-        // GenericCastType(myList2);
+        
 
-        for (int i = 0; i < myList2.size(); i++) {
-            GenericCastType(myList2.get(i));
-            // try {
-                // Integer castedValue = myList.get(i);
-            // } catch (ClassCastException e) {
-                // System.out.println("Didn't work");
-            // }
-        }
+        // for (int i = 0; i < myList2.size(); i++) {
+        //     // try {
+        //         // Integer castedValue = myList.get(i);
+        //     // } catch (ClassCastException e) {
+        //         // System.out.println("Didn't work");
+        //     // }
+        // }
 
         System.out.println(myList2);
 
